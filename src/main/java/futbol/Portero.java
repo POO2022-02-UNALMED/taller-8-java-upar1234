665ae53614e9a5a16965c8d8f782b71f23f94995
upar1234 <upar1234@gmail.com>
2022-10-18 13:41:06 -0500

    Update Portero.java

--- src/main/java/futbol/Portero.java
+++ src/main/java/futbol/Portero.java
@@ -2,20 +2,16 @@ package futbol;
 public class Portero extends Futbolista{
     public short golesRecibidos;
     public byte dorsal;
-
-    public Portero(String nombre, int edad, String posicion, short golesRecibidos, byte dorsal) {
-        super(nombre, edad, posicion);
-        this.golesRecibidos=golesRecibidos;
+    public Portero(String nombre, int edad, short golesRecibidos, byte dorsal) {
+        super(nombre,edad,"Portero");
         this.dorsal=dorsal;
-    }
+        this.golesRecibidos=golesRecibidos;}
 
-    @Override
-    public String toString(){
-        return("El futbolista "+getNombre()+" tiene "+getEdad()+ ", y juega de "+getPosicion()+" con el dorsal "+dorsal+ ". Le han marcado"+golesRecibidos);
-    }
-
+    public String toString() {
+        return "El futbolista "+getNombre()+" tiene "+getEdad()+ ", y juega de "+getPosicion()+" con el dorsal "+this.dorsal+ ". Le han marcado "+this.golesRecibidos;}
     public boolean jugarConLasManos() {
         return true;}
 
-}
-
+    public int compareTo(Object futbol) {
+        Portero port=(Portero)futbol;
+        return port.golesRecibidos-this.golesRecibidos;}}
