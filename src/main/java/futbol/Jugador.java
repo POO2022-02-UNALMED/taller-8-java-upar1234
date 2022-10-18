0d074582de77f356b51ee1a5684ebbceff718f67
upar1234 <upar1234@gmail.com>
2022-10-18 13:41:45 -0500

    j

--- src/main/java/futbol/Jugador.java
+++ src/main/java/futbol/Jugador.java
@@ -1,51 +1,24 @@
 package futbol;
-
 public class Jugador extends Futbolista {
-
     public short golesMarcados;
     public byte dorsal;
-
-    public Jugador(String n, int e, String p, short golesMarcados, byte dorsal) {
-        super(n, e, p);
-        this.golesMarcados = golesMarcados;
-        this.dorsal = dorsal;
-    }
+    public Jugador(String nombre, int edad, String posicion, short golesMarcados, byte dorsal) {
+        super(nombre,edad,posicion);
+        this.golesMarcados=golesMarcados;
+        this.dorsal=dorsal;}
 
     public Jugador() {
         super();
-        this.golesMarcados = 287;
-        this.dorsal = 7;
-    }
-
-    public byte getDorsal() {
-        return dorsal;
-    }
-
-    public void setDorsal(byte dorsal) {
-        this.dorsal = dorsal;
-    }
-
-    public short getGolesMarcados() {
-        return golesMarcados;
-    }
-
-    public void setGolesMarcados(short golesMarcados) {
-        this.golesMarcados = golesMarcados;
-    }
-
-
+        golesMarcados=289;
+        dorsal=7;}
     @Override
-    public String toString() {
-        return ("El futbolista " + getNombre() + " tiene " + getEdad() + ", y juega de " + getPosicion() + " con el dorsal " + dorsal + ". Ha marcado " + golesMarcados);
-    }
-
     public int compareTo(Object futbol) {
-        Jugador juga = (Jugador) futbol;
-        return this.getEdad() - juga.getEdad();
-    }
+        Jugador juga=(Jugador)futbol;
+        return this.getEdad()-juga.getEdad();}
 
     public boolean jugarConLasManos() {
-        return false;
-    }
+        return false;}
 
+    public String toString() {
+        return "El futbolista "+getNombre()+" tiene "+getEdad()+ ", y juega de "+getPosicion()+" con el dorsal "+this.dorsal+ ". Ha marcado "+this.golesMarcados;}
 }
